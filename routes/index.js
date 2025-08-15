920603303e86559c57ed6b0796a8c5bce4397ce0
RajaSahVayuz <raja.sah@vayuz.com>
2025-08-15 15:25:15 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ const userRoutes = require('./userRoutes.js');
 const mcqRoutes = require('./mcqRoutes.js');
 const masterRoutes = require('./masterRoutes.js');
 
-router.use("/user",userRoutes);
+router.use("/user", userRoutes);
 
 
-router.use("/mcq",mcqRoutes);
-router.use("/master",masterRoutes);
+router.use("/mcq", mcqRoutes);
+router.use("/master", masterRoutes);
 
 
 
 // Default Route
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     res.json({
-        test:"API health is ok"
+        test: "API health is ok"
     })
 });
 
