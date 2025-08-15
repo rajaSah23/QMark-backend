bb056071c43aeaf0438cf8009a38354d1f94d2a1
rajaSah23 <rajasah143rk@gmail.com>
2025-08-15 11:58:55 +0530

    setup

--- middleware/asyncErrorHandler.js
+++ middleware/asyncErrorHandler.js
@@ -2,7 +2,7 @@ const errorResponse = require("../utility/ErrorResponse");
 
 const asyncErrorHandler = (func) => {
     return (req, res, next) => {
-        func(req, res, next).catch(err =>errorResponse(res,err));
+        func(req, res, next).catch(err => errorResponse(res, err));
     }
 }
 
