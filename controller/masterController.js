8978095520e0aee8efbcb61e19149c0e73a77b45
rajaSah23 <rajasah143rk@gmail.com>
2025-09-04 21:33:55 +0530

    test

--- controller/masterController.js
+++ controller/masterController.js
@@ -3,12 +3,12 @@ const { successResponse } = require("../utility/successResponse");
 
 class MasterController {
     addSubjectAndTopics = async (req, res) => {
-        const response = await masterService.createSubjectAndTopic(req.user.id,req.body);
+        const response = await masterService.createSubjectAndTopic(req.user.id, req.body);
 
         res.status(201).json(successResponse(201, response, "Subject added"))
     }
     addTopic = async (req, res) => {
-        const response = await masterService.addTopic(req.user.id,req.body);
+        const response = await masterService.addTopic(req.user.id, req.body);
 
         res.status(201).json(successResponse(201, response, "Subject added"))
     }
@@ -28,17 +28,17 @@ class MasterController {
         res.status(200).json(successResponse(200, response, "Topic deleted"));
     }
     updateSubject = async (req, res) => {
-        const response = await masterService.updateSubjectById(req.user.id,req.params.subjectId,req.body);
+        const response = await masterService.updateSubjectById(req.user.id, req.params.subjectId, req.body);
 
         res.status(202).json(successResponse(202, response, "Subject  Updated"))
     }
     getTopics = async (req, res) => {
-        const response = await masterService.getTopicList(req.user.id,req.params.subjectId);
+        const response = await masterService.getTopicList(req.user.id, req.params.subjectId);
 
         res.status(200).json(successResponse(200, response, "Topic list sent"))
     }
     updateTopic = async (req, res) => {
-        const response = await masterService.updateTopicById(req.params.topicId,req.body);
+        const response = await masterService.updateTopicById(req.params.topicId, req.body);
 
         res.status(202).json(successResponse(202, response, "Topic Updated"))
     }
